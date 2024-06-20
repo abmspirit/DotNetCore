9320cd12baf1a9ac32477882a82f0f3d28292534
abmspirit <abm.spirit@gmail.com>
2024-06-20 15:25:42 +0530

    Initial commit of your project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+   
+    public string Index()
+    {
+        return "Hello";
+    }
+
+   
+
+   
+}
